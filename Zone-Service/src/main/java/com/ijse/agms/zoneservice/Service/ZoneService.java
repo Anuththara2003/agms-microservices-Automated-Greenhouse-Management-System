bf471bf01200bc70b408a670711dcfafab10ffc7
Sandaru Anuththara <sandaruperera108@gmail.com>
2026-03-12 16:03:57 +0530

    feat: implement Zone Management Service and configure API Gateway routing with JWT security

---
+++ Zone-Service/src/main/java/com/ijse/agms/zoneservice/Service/ZoneService.java
@@ -0,0 +1,14 @@
+package com.ijse.agms.zoneservice.Service;
+
+
+import com.ijse.agms.zoneservice.DTO.ZoneDTO;
+
+public interface ZoneService {
+    ZoneDTO saveZone(ZoneDTO zoneDTO);
+
+    ZoneDTO getZoneById(Long id);
+
+    String updateZone(Long id, ZoneDTO zoneDTO);
+
+    boolean deleteZone(Long id);
+}
